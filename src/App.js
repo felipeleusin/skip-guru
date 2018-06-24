1241692b993b28dc1dcd7e1d664c2aeac5c0865a
Felipe Amorim <felipe.leusin@gmail.com>
2018-06-24 16:56:35 +0000

    (feat) included react-router and moved Wizard to pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Provider as ThemeProvider, Toolbar, Root, Text } from 'rebass/emotion'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import theme from '~/theme'
 import store from '~/store'
 
-import Wizard from '~/components/Wizard'
+import Wizard from '~/pages/Wizard'
 
 const App = () => (
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Root css={{ height: '100vh' }}>
-        <Toolbar bg="red" color="white">
-          <Text fontWeight="bold">SkipGuru</Text>
-        </Toolbar>
-        <Wizard />
-      </Root>
-    </ThemeProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Root css={{ height: '100vh' }}>
+          <Toolbar bg="red" color="white">
+            <Text fontWeight="bold">SkipGuru</Text>
+          </Toolbar>
+          <Route exact path="/" component={Wizard} />
+        </Root>
+      </ThemeProvider>
+    </Provider>
+  </BrowserRouter>
 )
 
 export default App
